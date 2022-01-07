e1d2a8d8bdc07db80b79b1542ff544d711f3a5e2
unknown <romain.dabin@gmail.com>
2022-01-07 14:49:55 +0100

    ajout Dao uti, absence

---
+++ src/main/java/sopra/formation/dao/IAbsenceDao.java
@@ -0,0 +1,8 @@
+package sopra.formation.dao;
+
+import sopra.formation.model.Absence;
+
+public interface IAbsenceDao extends IDao<Absence, Long>{
+
+
+}
---
+++ src/main/java/sopra/formation/dao/IUtilisateurDao.java
@@ -0,0 +1,9 @@
+package sopra.formation.dao;
+
+import sopra.formation.model.Utilisateur;
+
+public interface IUtilisateurDao extends IDao<Utilisateur, Long>{
+	
+	
+
+}
