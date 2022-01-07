bd8003adb36c03a1f1b80c7e4342aedd905286de
unknown <william_frans@hotmail.com>
2022-01-07 14:59:16 +0100

    dao competence matiere cours

---
+++ src/main/java/sopra/formation/dao/ICours.java
@@ -0,0 +1,7 @@
+package sopra.formation.dao;
+
+import sopra.formation.model.Cours;
+
+public interface ICours extends IDao<Cours, Long>{
+
+}
---
+++ src/main/java/sopra/formation/dao/jpa/CoursDaoJpa.java
@@ -0,0 +1,157 @@
+package sopra.formation.dao.jpa;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.TypedQuery;
+
+import sopra.formation.Application;
+import sopra.formation.dao.ICours;
+import sopra.formation.model.Cours;
+
+
+public class CoursDaoJpa implements ICours{
+
+	@Override
+	public List<Cours> findAll() {
+		List<Cours> cours = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			TypedQuery<Cours> query = em.createQuery("select c from Cours c", Cours.class);
+
+			cours = query.getResultList();
+
+			tx.commit();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return cours;
+	}
+
+	@Override
+	public Cours findById(Long id) {
+		Cours cours = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			cours = em.find(Cours.class, id);
+
+			tx.commit();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return cours;
+	}
+
+	@Override
+	public Cours save(Cours obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			obj = em.merge(obj);
+
+			tx.commit();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return obj;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		delete(findById(id));
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.remove(em.find(Cours.class, id));
+
+			tx.commit();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+		
+	}
+
+	@Override
+	public void delete(Cours obj) {
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.remove(em.merge(obj));
+
+			tx.commit();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+		
+	}
+
+}
